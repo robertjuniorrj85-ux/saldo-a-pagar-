fdd3e553ee98f6e60088307de02edfd2224e75a5
robertjuniorrj85-ux <robertjunior.rj85@gmail.com>
2026-09-05 21:44:59 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='saldo-a-pagar-v6';
+const CACHE='saldo-a-pagar-v7';
 const STATIC=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
